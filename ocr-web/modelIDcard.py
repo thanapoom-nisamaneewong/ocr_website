f2b50d6dacd3c1c8ee4051c582aa42885b04b87a
thanapoom-nisamaneewong <74890654+thanapoom-nisamaneewong@users.noreply.github.com>
2022-04-28 16:32:50 +0700

    Update modelIDcard.py

--- ocr-web/modelIDcard.py
+++ ocr-web/modelIDcard.py
@@ -24,7 +24,7 @@ def readText(x, lang):
     #pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     #pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
     #pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-    #pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/5.1.0/bin/tesseract'
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/5.1.0/bin/tesseract'
 
     TH_config = ('-l tha --psm 6')
     EN_config = ('-l eng --psm 6')
